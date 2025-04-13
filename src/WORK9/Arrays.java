daeb110266a45e5a99b3f44f03c6c53dd370c7db
s.lesnyak <s.lesnyak@bft.ru>
2025-04-13 22:56:46 +0300

    Create job #9

--- src/WORK9/Arrays.java
+++ src/WORK9/Arrays.java
@@ -6,7 +6,7 @@ public class Arrays {
     public int getRandomString()
     {
         int a = (int) (Math.random() * 3); //генерим число
-        System.out.printf("Рандомное число - %d \n" ,a);
+        System.out.printf("Рандомное число - %d  \n" ,a);
         String b = arrays[a]; // выводим строку по сгенерированному числу
         System.out.printf("Строка \"%s\" по индексу %d \n" ,b ,a);
 
